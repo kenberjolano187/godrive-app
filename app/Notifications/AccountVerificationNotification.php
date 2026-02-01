1392ac28fe7a60f4a58ade72fad349a677e6c620
Ken Berjolano <kenberjolano187@gmail.com>
2026-02-01 20:44:48 +0800

    Disable fortify registration and remove queue for debugging

--- app/Notifications/AccountVerificationNotification.php
+++ app/Notifications/AccountVerificationNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AccountVerificationNotification extends Notification implements ShouldQueue
+class AccountVerificationNotification extends Notification
 {
     use Queueable;
 
