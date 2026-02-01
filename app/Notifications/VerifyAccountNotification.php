ccf74a7158847fadb1680b41c91e0eaf9de95bca
Ken Berjolano <kenberjolano187@gmail.com>
2026-02-01 21:05:57 +0800

    Disable fortify

--- app/Notifications/VerifyAccountNotification.php
+++ app/Notifications/VerifyAccountNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class VerifyAccountNotification extends Notification
+class VerifyAccountNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
